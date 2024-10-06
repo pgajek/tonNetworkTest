3ddc4a7a4e7cda49a9a5c86ff57b047911002f21
pgajek <pgajek94@gmail.com>
2024-10-06 13:53:18 +0200

    new game

--- tests/TonTut.spec.ts
+++ tests/TonTut.spec.ts
@@ -39,44 +39,6 @@ describe('TonTut', () => {
             // the check is done inside beforeEach
             // blockchain and tonTut are ready to use
         });
-        it('should increase counter', async () => {
-            const increaseTimes = 3;
-            for(let i = 0; i < increaseTimes; i++){
-                console.log(`increase ${i+1}/${increaseTimes}`);
-    
-                const increaser = await blockchain.treasury('increaser' + i);
-    
-                const counterBefore = await tonTut.getCounter();
-                console.log("Counter before", counterBefore);
-    
-                const increaseBy = BigInt(Math.floor(Math.random() * 100));
-    
-                console.log(`increasing by ${increaseBy}`);
-    
-                const increaseResult = await tonTut.send(
-                    increaser.getSender(),
-                    {
-                        value: toNano('0.05'),
-                    },
-                    {
-                        $$type: "Add",
-                        queryId: 0n,
-                        amount: increaseBy
-                    }
-                );
-                expect(increaseResult.transactions).toHaveTransaction({
-                    from: increaser.address,
-                    to: tonTut.address,
-                    success: true,
-                });
-    
-                const counterAfter = await tonTut.getCounter();
-                console.log("Counter after", counterAfter);
-    
-                expect(counterAfter).toBe(counterBefore + increaseBy);
-                
-            }
-        });
         it('should change owner', async () => {
             const newOwner = await blockchain.treasury('newOwner');
     
