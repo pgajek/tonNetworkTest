ae702c436932deb4626855c64f95edb88e49911c
pgajek <pgajek94@gmail.com>
2024-10-08 14:46:16 +0200

    working functionality

--- tests/TonTut.spec.ts
+++ tests/TonTut.spec.ts
@@ -1,26 +1,26 @@
-import { Blockchain, printTransactionFees, SandboxContract, TreasuryContract } from '@ton/sandbox';
-import { Address, toNano } from '@ton/core';
+import { Blockchain,  SandboxContract, TreasuryContract } from '@ton/sandbox';
+import {  toNano } from '@ton/core';
 import { TonTut } from '../wrappers/TonTut';
 import '@ton/test-utils';
-import {createHash} from 'node:crypto';
+import { createHash } from 'node:crypto';
 
 describe('TonTut', () => {
     let blockchain: Blockchain;
     let deployer: SandboxContract<TreasuryContract>;
-    let signer: SandboxContract<TreasuryContract>;
     let tonTut: SandboxContract<TonTut>;
+    let signer: SandboxContract<TreasuryContract>;
     let player1: SandboxContract<TreasuryContract>;
     let player2: SandboxContract<TreasuryContract>;
 
     beforeEach(async () => {
         blockchain = await Blockchain.create();
 
-        tonTut = blockchain.openContract(await TonTut.fromInit(1n));
-
         deployer = await blockchain.treasury('deployer');
-        signer = await blockchain.treasury('signer');
         player1 = await blockchain.treasury('player1');
         player2 = await blockchain.treasury('player2');
+        signer = await blockchain.treasury('signer');
+
+        tonTut = blockchain.openContract(await TonTut.fromInit(1n, deployer.address));
 
         const deployResult = await tonTut.send(
             deployer.getSender(),
@@ -46,12 +46,12 @@ describe('TonTut', () => {
             // the check is done inside beforeEach
             // blockchain and tonTut are ready to use
         });
-        it('should change owner', async () => {
-            const newOwner = await blockchain.treasury('newOwner');
+        it('should change signer', async () => {
+            const newSigner = await blockchain.treasury('newSigner');
     
-            const ownerBefore = await tonTut.getOwner();
+            const signerBefore = await tonTut.getOwner();
             
-            expect(ownerBefore.toString()).toBe(deployer.address.toString());
+            expect(signerBefore.toString()).toBe(deployer.address.toString());
     
             const changeOwnerResult = await tonTut.send(
                 deployer.getSender(),
@@ -59,9 +59,8 @@ describe('TonTut', () => {
                     value: toNano('0.05'),
                 },
                 {
-                    $$type: "ChangeOwner",
-                    queryId: 0n,
-                    newOwner: newOwner.address,
+                    $$type: "ChangeSignerMessage",
+                    newSigner: newSigner.address,
                 }
             );
     
@@ -71,9 +70,9 @@ describe('TonTut', () => {
                 success: true,
             });
     
-            const ownerAfter = await tonTut.getOwner();
+            const signerAfter = await tonTut.getGetSigner();
     
-            expect(ownerAfter).toEqualAddress(newOwner.address);
+            expect(signerAfter).toEqualAddress(newSigner.address);
         });
     })
     describe('Tree functionality', ()=>{
@@ -143,7 +142,7 @@ describe('TonTut', () => {
             const getRaffle = await tonTut.getGetRaffle(BigInt(0));
             expect(getRaffle!.randomnessCommitment).toBe(randomnessCommitment);
         });
-        it('should correctly apply tickets to players accound', async () => {
+        it('should correctly apply tickets to players account', async () => {
             const randomnessInput = "test1";
             const randomnessCommitment = BigInt('0x' + createHash('sha256').update(randomnessInput).digest('hex'));
             
@@ -180,21 +179,418 @@ describe('TonTut', () => {
                 }
             );
 
-            const getCheckerState = await tonTut.getCheckerState();
-            console.log("getCheckerState", getCheckerState);
+            expect(getTicketsResult.transactions).toHaveTransaction({
+                from: player1.address,
+                to: tonTut.address,
+                success: true,
+            });
 
-            // expect(getTicketsResult.transactions).toHaveTransaction({
-            //     from: player1.address,
-            //     to: tonTut.address,
-            //     success: true,
-            // });
+            const playerAmount = await tonTut.getGetPlayerAmount(BigInt(0), player1.address);
+            expect(playerAmount).toBe(BigInt(1));
+        });
+        it('should correctly get raffle randomness commitment', async () => {
+             const randomnessInput = "test";
+            const randomnessCommitment = BigInt('0x' + createHash('sha256').update(randomnessInput).digest('hex'));
+  
+            const createRaffleResult = await tonTut.send(
+                deployer.getSender(),
+                {
+                    value: toNano('0.05'),
+                },
+                {
+                    $$type: "CreateNewRaffleMessage",
+                    randomnessCommitment: randomnessCommitment,
+                    key: BigInt(0)
+                }
+            );
 
-            console.log(printTransactionFees(getTicketsResult.transactions));
-            // const playerAmount = await tonTut.getGetPlayerAmount(BigInt(0), player1.address);
-            // expect(playerAmount).toBe(BigInt(1));
+            expect(createRaffleResult.transactions).toHaveTransaction({
+                from: deployer.address,
+                to: tonTut.address,
+                success: true,
+            });
+
+            const getRaffleRandomnessCommitment = await tonTut.getGetRaffleRandomnessCommitment(BigInt(0));
+            expect(getRaffleRandomnessCommitment).toBe(randomnessCommitment);
+        });
+        it('should correctly get raffle current randomness', async () => {
+             const randomnessInput = "test";
+            const randomnessCommitment = BigInt('0x' + createHash('sha256').update(randomnessInput).digest('hex'));
+  
+            const createRaffleResult = await tonTut.send(
+                deployer.getSender(),
+                {
+                    value: toNano('0.05'),
+                },
+                {
+                    $$type: "CreateNewRaffleMessage",
+                    randomnessCommitment: randomnessCommitment,
+                    key: BigInt(0)
+                }
+            );
+
+            expect(createRaffleResult.transactions).toHaveTransaction({
+                from: deployer.address,
+                to: tonTut.address,
+                success: true,
+            });
+
+            const raffleCurrentRandomness = await tonTut.getGetRaffleCurrentRandomness(BigInt(0));
+            expect(raffleCurrentRandomness).toBe(randomnessCommitment);
+        });
+        it('should correctly get raffle commitment opened after creating new game', async () => {
+             const randomnessInput = "test";
+            const randomnessCommitment = BigInt('0x' + createHash('sha256').update(randomnessInput).digest('hex'));
+  
+            const createRaffleResult = await tonTut.send(
+                deployer.getSender(),
+                {
+                    value: toNano('0.05'),
+                },
+                {
+                    $$type: "CreateNewRaffleMessage",
+                    randomnessCommitment: randomnessCommitment,
+                    key: BigInt(0)
+                }
+            );
+
+            expect(createRaffleResult.transactions).toHaveTransaction({
+                from: deployer.address,
+                to: tonTut.address,
+                success: true,
+            });
+
+            const raffleCommitmentOpened = await tonTut.getGetRaffleCommitmentOpened(BigInt(0));
+            expect(raffleCommitmentOpened).toBe(false);
+        });
+        it('should correctly get correctly get current raffle total sum', async () => {
+             const randomnessInput = "test";
+            const randomnessCommitment = BigInt('0x' + createHash('sha256').update(randomnessInput).digest('hex'));
+  
+            const createRaffleResult = await tonTut.send(
+                deployer.getSender(),
+                {
+                    value: toNano('0.05'),
+                },
+                {
+                    $$type: "CreateNewRaffleMessage",
+                    randomnessCommitment: randomnessCommitment,
+                    key: BigInt(0)
+                }
+            );
+
+            expect(createRaffleResult.transactions).toHaveTransaction({
+                from: deployer.address,
+                to: tonTut.address,
+                success: true,
+            });
+
+            const getTicketsResult = await tonTut.send(
+                player1.getSender(),
+                {
+                    value: toNano('0.15'),
+                },
+                {
+                    $$type: "GetRaffleTicketsMessage",
+                    raffleId: BigInt(0),
+                    account: player1.address,
+                    amount: BigInt(1),
+                    nonce: BigInt(0),
+                    randomness: BigInt(100)    
+                }
+            );
+
+            expect(getTicketsResult.transactions).toHaveTransaction({
+                from: player1.address,
+                to: tonTut.address,
+                success: true,
+            });
+
+            const totalSum = await tonTut.getGetTotalSum(BigInt(0));
+            expect(totalSum).toBe(BigInt(1));
+
+            const getTicketsResult2 = await tonTut.send(
+                player1.getSender(),
+                {
+                    value: toNano('0.15'),
+                },
+                {
+                    $$type: "GetRaffleTicketsMessage",
+                    raffleId: BigInt(0),
+                    account: player2.address,
+                    amount: BigInt(14),
+                    nonce: BigInt(0),
+                    randomness: BigInt(100)    
+                }
+            );
+
+            expect(getTicketsResult2.transactions).toHaveTransaction({
+                from: player1.address,
+                to: tonTut.address,
+                success: true,
+            });
+
+            const totalSum2 = await tonTut.getGetTotalSum(BigInt(0));
+            expect(totalSum2).toBe(BigInt(15));
+        });
+        it('should get winner', async () => {
+            const randomnessInput = "test";
+            const randomnessCommitment = BigInt("0x" + createHash('sha256').update(randomnessInput).digest('hex'));
+
+            const createRaffleResult = await tonTut.send(
+                deployer.getSender(),
+                {
+                    value: toNano('0.05'),
+                },
+                {
+                    $$type: "CreateNewRaffleMessage",
+                    randomnessCommitment: randomnessCommitment,
+                    key: BigInt(0)
+                }
+            );
+
+            expect(createRaffleResult.transactions).toHaveTransaction({
+                from: deployer.address,
+                to: tonTut.address,
+                success: true,
+            });
+
+            const getTicketsResult = await tonTut.send(
+                player1.getSender(),
+                {
+                    value: toNano('0.15'),
+                },
+                {
+                    $$type: "GetRaffleTicketsMessage",
+                    raffleId: BigInt(0),
+                    account: player1.address,
+                    amount: BigInt(1),
+                    nonce: BigInt(0),
+                    randomness: BigInt(100)    
+                }
+            );
+
+            expect(getTicketsResult.transactions).toHaveTransaction({
+                from: player1.address,
+                to: tonTut.address,
+                success: true,
+            });
+
+            const totalSum = await tonTut.getGetTotalSum(BigInt(0));
+            expect(totalSum).toBe(BigInt(1));
+
+            const getTicketsResult2 = await tonTut.send(
+                player1.getSender(),
+                {
+                    value: toNano('0.15'),
+                },
+                {
+                    $$type: "GetRaffleTicketsMessage",
+                    raffleId: BigInt(0),
+                    account: player2.address,
+                    amount: BigInt(14),
+                    nonce: BigInt(0),
+                    randomness: BigInt(100)    
+                }
+            );
+
+            expect(getTicketsResult2.transactions).toHaveTransaction({
+                from: player1.address,
+                to: tonTut.address,
+                success: true,
+            });
+
+            const totalSum2 = await tonTut.getGetTotalSum(BigInt(0));
+            expect(totalSum2).toBe(BigInt(15));
+            const randomnessOpening = randomnessInput;
+
+            const winnerResult = await tonTut.getGetWinner(BigInt(0));
+            expect(winnerResult).toBeNull();
+
+            const getWinnerResult = await tonTut.send(
+                deployer.getSender(),
+                {
+                    value: toNano('0.15'),
+                },
+                {
+                    $$type: "GetWinnerMessage",
+                    raffleId: BigInt(0),
+                    randomnessOpening:randomnessOpening   
+                }
+            );
+
+            expect(getWinnerResult.transactions).toHaveTransaction({
+                from: deployer.address,
+                to: tonTut.address,
+                success: true,
+            });
+
+            const winner = await tonTut.getGetWinner(BigInt(0));
+            console.log(winner);
+            expect(winner).not.toBeNull();
+        });
+        it('should fail with invalid randomnes opening', async () => {
+            const randomnessInput = "test";
+            const randomnessCommitment = BigInt("0x" + createHash('sha256').update(randomnessInput).digest('hex'));
+
+            const createRaffleResult = await tonTut.send(
+                deployer.getSender(),
+                {
+                    value: toNano('0.05'),
+                },
+                {
+                    $$type: "CreateNewRaffleMessage",
+                    randomnessCommitment: randomnessCommitment,
+                    key: BigInt(0)
+                }
+            );
+
+            expect(createRaffleResult.transactions).toHaveTransaction({
+                from: deployer.address,
+                to: tonTut.address,
+                success: true,
+            });
+
+            const randomnessOpening = 'wrong input';
+
+            const getWinnerResult = await tonTut.send(
+                deployer.getSender(),
+                {
+                    value: toNano('0.15'),
+                },
+                {
+                    $$type: "GetWinnerMessage",
+                    raffleId: BigInt(0),
+                    randomnessOpening:randomnessOpening   
+                }
+            );
+
+            expect(getWinnerResult.transactions).toHaveTransaction({
+                from: deployer.address,
+                to: tonTut.address,
+                success: false,
+            });
+
+            const winner = await tonTut.getGetWinner(BigInt(0));
+            expect(winner).toBeNull();
+        });
+        it('should not get tickets if commitment already opened', async () => {
+            const randomnessInput = "test";
+            const randomnessCommitment = BigInt("0x" + createHash('sha256').update(randomnessInput).digest('hex'));
+
+            const createRaffleResult = await tonTut.send(
+                deployer.getSender(),
+                {
+                    value: toNano('0.05'),
+                },
+                {
+                    $$type: "CreateNewRaffleMessage",
+                    randomnessCommitment: randomnessCommitment,
+                    key: BigInt(0)
+                }
+            );
+
+            expect(createRaffleResult.transactions).toHaveTransaction({
+                from: deployer.address,
+                to: tonTut.address,
+                success: true,
+            });
+
+            const getTicketsResult = await tonTut.send(
+                player1.getSender(),
+                {
+                    value: toNano('0.15'),
+                },
+                {
+                    $$type: "GetRaffleTicketsMessage",
+                    raffleId: BigInt(0),
+                    account: player1.address,
+                    amount: BigInt(1),
+                    nonce: BigInt(0),
+                    randomness: BigInt(100)    
+                }
+            );
+
+            expect(getTicketsResult.transactions).toHaveTransaction({
+                from: player1.address,
+                to: tonTut.address,
+                success: true,
+            });
+
+            const totalSum = await tonTut.getGetTotalSum(BigInt(0));
+            expect(totalSum).toBe(BigInt(1));
+
+            const getTicketsResult2 = await tonTut.send(
+                player1.getSender(),
+                {
+                    value: toNano('0.15'),
+                },
+                {
+                    $$type: "GetRaffleTicketsMessage",
+                    raffleId: BigInt(0),
+                    account: player2.address,
+                    amount: BigInt(14),
+                    nonce: BigInt(0),
+                    randomness: BigInt(100)    
+                }
+            );
+
+            expect(getTicketsResult2.transactions).toHaveTransaction({
+                from: player1.address,
+                to: tonTut.address,
+                success: true,
+            });
+
+            const totalSum2 = await tonTut.getGetTotalSum(BigInt(0));
+            expect(totalSum2).toBe(BigInt(15));
+            const randomnessOpening = randomnessInput;
+
+            const winnerResult = await tonTut.getGetWinner(BigInt(0));
+            expect(winnerResult).toBeNull();
+
+            const getWinnerResult = await tonTut.send(
+                deployer.getSender(),
+                {
+                    value: toNano('0.15'),
+                },
+                {
+                    $$type: "GetWinnerMessage",
+                    raffleId: BigInt(0),
+                    randomnessOpening:randomnessOpening   
+                }
+            );
+
+            expect(getWinnerResult.transactions).toHaveTransaction({
+                from: deployer.address,
+                to: tonTut.address,
+                success: true,
+            });
+
+            const winner = await tonTut.getGetWinner(BigInt(0));
+            expect(winner).not.toBeNull();
+
+            const getTicketsResultAfterCommitmentOpened = await tonTut.send(
+                player1.getSender(),
+                {
+                    value: toNano('0.15'),
+                },
+                {
+                    $$type: "GetRaffleTicketsMessage",
+                    raffleId: BigInt(0),
+                    account: player1.address,
+                    amount: BigInt(1),
+                    nonce: BigInt(0),
+                    randomness: BigInt(100)    
+                }
+            );
+
+            expect(getTicketsResultAfterCommitmentOpened.transactions).toHaveTransaction({
+                from: player1.address,
+                to: tonTut.address,
+                success: false,
+            });
         });
         // it('should fail creating new game if signarute is not valid', async () => {});
-        // it('correctly increases player amount', async () => {});
         // it('fails increasing player amount if signature is not valid', async () => {});
         // it('should return correct winner', async () => {});
         // it('should return correct winner with boundary values', async () => {});
